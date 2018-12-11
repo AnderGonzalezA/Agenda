fe8c167ce3fc72332e949403be4a72e2f7db8206
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2018-12-11 13:58:25 +0100

    11/12 last

--- src/com/zubiri/agenda/Agenda.java
+++ src/com/zubiri/agenda/Agenda.java
@@ -2,7 +2,7 @@
 * The Agenda program implements a class that
 * can be used to create agenda objects which will hold contact's information.
 *
-* @author  AnderGonzalez
+* @author  Ander Gonzalez
 * @version 1.0
 * @since   2018-11-12
 */
@@ -17,6 +17,15 @@ public class Agenda {
 	public void setContactArrayList(ArrayList<Contact> contactArrayList){
 		this.contactArrayList=contactArrayList;
 	}
+	/**
+
+     * Method that adds a new contact to the ArrayList "contactArrayList"
+
+     * @param person The person whose contact will be added
+     * @param mobileNum The mobile number of the contact
+     * @param address The address of the contact
+
+     */
 	public void addContact(Person person,int mobileNum,String address) {
 		boolean contactFound=false;
 		for (int i=0;i<contactArrayList.size();i++) {
@@ -29,6 +38,15 @@ public class Agenda {
 			contactArrayList.add(new Contact(person,mobileNum,address));
 		}
 	}
+	/**
+
+     * Method that modifies a contact from the ArrayList "contactArrayList"
+
+     * @param name The name of the person whose contact's information will be modified 
+     * @param mobileNum The new mobile number of the contact
+     * @param address The new address of the contact
+
+     */
 	public void modifyContact(String name,int mobileNum,String address) {
 		for (int i=0;i<contactArrayList.size();i++) {
 			if (contactArrayList.get(i).getPerson().getName().equals(name)) {
@@ -38,6 +56,13 @@ public class Agenda {
 			}
 		}
 	}
+	/**
+
+     * Method that Removes a contact from the ArrayList "contactArrayList"
+
+     * @param name The name of the person whose contact will be removed
+
+     */
 	public void removeContact(String name) {
 		for (int i=0;i<contactArrayList.size();i++) {
 			if (contactArrayList.get(i).getPerson().getName().equals(name)) {
@@ -46,6 +71,14 @@ public class Agenda {
 			}
 		}
 	}
+	/**
+
+     * Method that returns the information of a contact from the ArrayList "contactArrayList"
+
+     * @param name The name of the person whose contact's information will be returned
+     * @return 
+
+     */
 	public String showContact(String name){
 		String information = "";
 		for (int i=0;i<contactArrayList.size();i++) {
--- src/com/zubiri/agenda/Contact.java
+++ src/com/zubiri/agenda/Contact.java
@@ -2,7 +2,7 @@
 * The Contact program implements a class that
 * can be used to create contact objects which will hold people's mobile number and address and their methods.
 *
-* @author  AnderGonzalez
+* @author  Ander Gonzalez
 * @version 1.0
 * @since   2018-11-12 
 */
@@ -12,11 +12,21 @@ public class Contact {
 	private Person person;
 	private int mobileNum = 0;
 	private String address = "";
+	/**
+
+     * Contact constructor
+
+     * @param person The person whose contact will be created
+     * @param mobileNum The mobile number of the contact
+     * @param address The address of the contact
+
+     */
 	public Contact(Person person, int mobileNum, String address){
 		this.person=person;
 		this.mobileNum=mobileNum;
 		this.address=address;
 	}
+	
 	public Person getPerson() {
 		return this.person;
 	}
